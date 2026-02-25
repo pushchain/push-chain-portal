07e7d7133cecfe3d79c5708380920f62a1f8487d
Riyanshu Patro <riyanshu@push.org>
2026-02-25 16:03:30 +0530

    chore: remove vite plugin require

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import vitePluginRequire from "vite-plugin-require";
 import svgr from "vite-plugin-svgr";
 import topLevelAwait from "vite-plugin-top-level-await";
 import viteTsconfigPaths from "vite-tsconfig-paths";
@@ -11,49 +10,16 @@ import { getPreviewBasePath } from "./basePath";
 
 export default defineConfig({
   plugins: [
+    topLevelAwait(),
     react(),
+    svgr(),
+    viteTsconfigPaths({ root: "./" }),
     nodePolyfills(),
   ],
 
-  resolve: {
-    dedupe: [
-      "@walletconnect/ethereum-provider",
-      "@walletconnect/modal",
-      "@walletconnect/sign-client",
-      "@walletconnect/universal-provider",
-      "@walletconnect/core",
-      "@walletconnect/utils",
-      "@walletconnect/jsonrpc-provider",
-      "@walletconnect/jsonrpc-utils",
-
-      "@web3modal/common",
-      "@web3modal/core",
-      "@web3modal/ui",
-
-      "lit",
-      "lit-html",
-      "lit-element",
-      "@lit/reactive-element",
-      "@lit-labs/motion",
-    ],
-  },
-
-  optimizeDeps: {
-    include: [
-      "@walletconnect/ethereum-provider",
-      "@walletconnect/modal",
-      "@walletconnect/sign-client",
-      "@walletconnect/universal-provider",
-      "@walletconnect/core",
-      "@walletconnect/utils",
-
-      "@web3modal/common",
-      "@web3modal/core",
-      "@web3modal/ui",
-
-      "lit",
-      "@lit/reactive-element",
-    ],
+  define: {
+    global: "globalThis",
+    historyApiFallback: true,
   },
 
   server: {
