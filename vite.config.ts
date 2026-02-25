3b8908dfe4ebc7f75f4babebdac09d9307bfcdec
Riyanshu Patro <riyanshu@push.org>
2026-02-25 20:41:40 +0530

    chore: quick check

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import svgr from "vite-plugin-svgr";
-import topLevelAwait from "vite-plugin-top-level-await";
-import viteTsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 import { getPreviewBasePath } from "./basePath";
 
 export default defineConfig({
   plugins: [
-    topLevelAwait(),
     react(),
-    svgr(),
-    viteTsconfigPaths({ root: "./" }),
     nodePolyfills(),
   ],
 
