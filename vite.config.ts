78f1de039fa92af2ee8880bb1f292c74bc9d55df
Riyanshu Patro <riyanshu@push.org>
2026-02-25 21:06:10 +0530

    chore: quick check

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import svgr from "vite-plugin-svgr";
-import topLevelAwait from "vite-plugin-top-level-await";
-import viteTsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 import { getPreviewBasePath } from "./basePath";
@@ -66,7 +63,9 @@ export default defineConfig({
     sourcemap: true,
     minify: false,
 
-    commonjsOptions: { transformMixedEsModules: true, },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
 
     rollupOptions: {
       input: {
