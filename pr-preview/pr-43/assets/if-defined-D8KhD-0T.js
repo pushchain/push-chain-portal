ed09e90a6e5100bb9bb71385587a0ef0cdbac4b7
corlard3y <corlard3y@users.noreply.github.com>
2026-04-04 16:24:05 +0000

    deploy: d42dd5f38c4a368954f3e9f7756c941cd202d5be

---
+++ pr-preview/pr-43/assets/if-defined-D8KhD-0T.js
@@ -0,0 +1,6 @@
+import{dk as r}from"./main-DKv60z-4.js";/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const m=o=>o??r;export{m as o};
+//# sourceMappingURL=if-defined-D8KhD-0T.js.map
---
+++ pr-preview/pr-43/assets/transactions-kvYD9hvv.js
@@ -0,0 +1,17 @@
+import{dn as f,dl as a,dm as m}from"./main-DKv60z-4.js";import{a as p}from"./index-CQTdKBQr.js";import"./index-B5kUqdjx.js";import"./class-map-DvZczRcV.js";import"./index-BLdMSmfL.js";import"./if-defined-D8KhD-0T.js";import"./index-DR20XTJ0.js";import"./index-Qs4G5eLq.js";import"./index-DjHDKRJ7.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var s=o.length-1;s>=0;s--)(l=o[s])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let c=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};c.styles=d;c=u([p("w3m-transactions-view")],c);export{c as W3mTransactionsView};
+//# sourceMappingURL=transactions-kvYD9hvv.js.map
