a1d5f9f5de53c66ac43d8498a467b7ed2b02e59f
corlard3y <corlard3y@users.noreply.github.com>
2026-04-06 10:51:36 +0000

    deploy: 9c87a505332669bd3be81780da87379551fe74af

---
+++ pr-preview/pr-43/assets/transactions-CtOO4txc.js
@@ -0,0 +1,17 @@
+import{dn as f,dl as a,dm as m}from"./main-NZo91ZD4.js";import{a as p}from"./index-DJ40bZEv.js";import"./index-CdPohHFX.js";import"./class-map-DcTKy-In.js";import"./index-kQp1VaK4.js";import"./if-defined-BnnQIipf.js";import"./index-BdkhkuRg.js";import"./index-D0LtE8Sm.js";import"./index-9udxK0Bm.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var s=o.length-1;s>=0;s--)(l=o[s])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let c=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};c.styles=d;c=u([p("w3m-transactions-view")],c);export{c as W3mTransactionsView};
+//# sourceMappingURL=transactions-CtOO4txc.js.map
