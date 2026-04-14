7c31fde13c35990abfda4692af5220af77b90010
corlard3y <corlardey@gmail.com>
2026-04-14 12:42:08 +0100

    chore: changes url

--- src/queries/baseURL.ts
+++ src/queries/baseURL.ts
@@ -21,7 +21,9 @@ export const getRewardsBaseURL = () => {
 export const getSeasonThreeBaseURL = () => {
   switch (appEnv) {
     case "prod":
-      return `https://us-east1-push-prod-apps.cloudfunctions.net/pushpointsrewardsystem`;
+      // TODO: update this before deploy
+      return `https://us-east1-push-dev-apps.cloudfunctions.net/helloWorld`;
+      // return `https://us-east1-push-prod-apps.cloudfunctions.net/pushpointsrewardsystem`;
     case "staging":
       return `https://us-east1-push-dev-apps.cloudfunctions.net/helloWorld`;
     case "dev":
