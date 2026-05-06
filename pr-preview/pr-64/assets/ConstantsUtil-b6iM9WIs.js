88e1847b57cba4b707f81fc626aa46b4e52078dd
corlard3y <corlard3y@users.noreply.github.com>
2026-05-06 13:47:12 +0000

    Deploy preview for PR 64 🛫

---
+++ pr-preview/pr-64/assets/ConstantsUtil-b6iM9WIs.js
@@ -0,0 +1,2 @@
+import{dj as t}from"./main-DVd03jb1.js";var e={};const o={ACCOUNT_TABS:[{label:"Tokens"},{label:"Activity"}],SECURE_SITE_ORIGIN:(typeof t<"u"&&typeof e<"u"?e.NEXT_PUBLIC_SECURE_SITE_ORIGIN:void 0)||"https://secure.walletconnect.org",VIEW_DIRECTION:{Next:"next",Prev:"prev"},ANIMATION_DURATIONS:{HeaderText:120},VIEWS_WITH_LEGAL_FOOTER:["Connect","ConnectWallets","OnRampTokenSelect","OnRampFiatSelect","OnRampProviders"],VIEWS_WITH_DEFAULT_FOOTER:["Networks"]};export{o as C};
+//# sourceMappingURL=ConstantsUtil-b6iM9WIs.js.map
---
+++ pr-preview/pr-64/assets/transactions-CB_Mk6Rd.js
@@ -0,0 +1,17 @@
+import{dn as f,dl as a,dm as m}from"./main-DVd03jb1.js";import{a as p}from"./index-AuXzr_58.js";import"./index-cfLuImLn.js";import"./class-map-us28KYSL.js";import"./index-Cd4c1iUE.js";import"./if-defined-BnfBoBng.js";import"./index-BEtsQ9fY.js";import"./index-DG9T4yUp.js";import"./index-HIXTrMl2.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var s=o.length-1;s>=0;s--)(l=o[s])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let c=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};c.styles=d;c=u([p("w3m-transactions-view")],c);export{c as W3mTransactionsView};
+//# sourceMappingURL=transactions-CB_Mk6Rd.js.map
