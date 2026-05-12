e07ec07464fd05549126f15ee460078c8dd423f1
corlard3y <corlardey@gmail.com>
2026-05-12 21:19:51 +0100

    chore: update quests

--- src/components/Rewards/utils/skipVerificationActivities.ts
+++ src/components/Rewards/utils/skipVerificationActivities.ts
@@ -1,4 +1,4 @@
-const SKIP_VERIFICATION_PREFIXES = ['unichess_', 'push_ninja_', 'moleswap_', 'boss_','bonus_non_cult_share_on_x_xp_boost'];
+const SKIP_VERIFICATION_PREFIXES = ['ramen_swap_', 'pusd_', 'moleswap_', 'boss_','bonus_non_cult_share_on_x_xp_boost'];
 
 export const shouldSkipVerification = (activityTypeId: string): boolean =>
   SKIP_VERIFICATION_PREFIXES.some((prefix) => activityTypeId?.startsWith(prefix));
