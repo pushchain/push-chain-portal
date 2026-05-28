4ff7e2f58a198f275b1721bf8f8816f502ea555d
corlard3y <corlardey@gmail.com>
2026-05-28 12:00:40 +0100

    chore: update quests reset-time

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 import svgr from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
-import { buildoptimize} from "ts-build-optimize";
 import { getPreviewBasePath } from "./basePath";
 
 export default defineConfig({
@@ -14,7 +13,6 @@ export default defineConfig({
     svgr(),
     viteTsconfigPaths({ root: "./" }),
     nodePolyfills(),
-    buildoptimize(),
   ],
 
   define: {
