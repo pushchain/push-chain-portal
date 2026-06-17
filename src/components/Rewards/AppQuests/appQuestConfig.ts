f14f820151f5ea67e1dbf6700f9434c5dc297d63
corlard3y <corlardey@gmail.com>
2026-06-17 17:07:53 +0100

    chore: update quest config

--- src/components/Rewards/AppQuests/appQuestConfig.ts
+++ src/components/Rewards/AppQuests/appQuestConfig.ts
@@ -5,6 +5,7 @@ import lastOneBg from '../../../../static/assets/website/rewards/last-one-bg.web
 import moleswapBg from '../../../../static/assets/website/rewards/moleswap-bg.webp';
 import bridgeBg from '../../../../static/assets/website/rewards/bridgeapp-bg.webp';
 import pushNinjaBg from '../../../../static/assets/website/rewards/Push-Ninja-bg.webp';
+import degenChessBg from '../../../../static/assets/website/rewards/Degen-Chess-bg.webp';
 
 export type AppQuestMetadata = {
   appName: string;
@@ -31,35 +32,44 @@ export const APP_QUEST_METADATA: Record<string, AppQuestMetadata> = {
   },
   "ramen-swap": {
     appName: "Ramen Swap",
-    appUrl: "",
+    appUrl: "ramenfi.xyz",
     bgImage: ramenBg,
+    titleGradient: "linear-gradient(180deg, #000 16.15%, #ED2027 89.06%)",
+    linkColor: "#DB2D33",
   },
   lastone: {
     appName: "Last One",
-    appUrl: "",
+    appUrl: "lastone.fun",
     bgImage: lastOneBg,
+    titleGradient: "linear-gradient(180deg, #000 16.15%, #6B30B2 89.06%);",
+    linkColor: "#653468",
   },
   moleswap: {
     appName: "MoleSwap",
-    appUrl: "",
+    appUrl: "moleswap.com",
     bgImage: moleswapBg,
+    titleGradient: "linear-gradient(180deg, #000 16.15%, #5562F3 89.06%)",
+    linkColor: "#184581",
   },
   "push-chain-bridge": {
     appName: "Push Chain Bridge",
-    appUrl: "",
+    appUrl: "bridge.push.org",
     bgImage: bridgeBg,
+    titleGradient: "linear-gradient(180deg, #000 16.15%, #9C60E4 89.06%)",
+    linkColor: "#5A1881",
   },
   "push-ninja": {
     appName: "Push Ninja",
-    appUrl: "",
+    appUrl: "pushninja.fun",
     bgImage: pushNinjaBg,
+    titleGradient: "linear-gradient(180deg, #000 16.15%, #B53838 89.06%)",
+    linkColor: "#771B1D",
   },
   unichess: {
-    appName: "Unichess",
-    appUrl: "",
-  },
-  cetra: {
-    appName: "Cetra",
-    appUrl: "",
+    appName: "Degen Chess",
+    appUrl: "degenchess.fun",
+    bgImage: degenChessBg,
+    titleGradient: "linear-gradient(180deg, #000 16.15%, #40278D 89.06%)",
+    linkColor: "#40278D",
   },
 };
