774c5d80e4639702fb8c5fd7399a5d9e0252332b
corlard3y <corlardey@gmail.com>
2026-06-23 12:59:27 +0100

    chore: update quests

--- src/components/Rewards/utils/skipVerificationActivities.ts
+++ src/components/Rewards/utils/skipVerificationActivities.ts
@@ -1,4 +1,4 @@
-const SKIP_VERIFICATION_PREFIXES = ['zappi_', 'pusd_', 'moleswap_', 'boss_','bonus_non_cult_share_on_x_xp_boost'];
+const SKIP_VERIFICATION_PREFIXES = ['zappi_', 'lastone_', 'moleswap_', 'boss_','bonus_non_cult_share_on_x_xp_boost'];
 
 export const shouldSkipVerification = (activityTypeId: string): boolean =>
   SKIP_VERIFICATION_PREFIXES.some((prefix) => activityTypeId?.startsWith(prefix));
