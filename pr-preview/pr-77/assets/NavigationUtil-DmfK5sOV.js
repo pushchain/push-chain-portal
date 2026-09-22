9132a06f76e7149d7656ec7230b173f61733c6e3
riyanshu-patro <riyanshu-patro@users.noreply.github.com>
2026-09-22 13:37:19 +0000

    Deploy preview for PR 77 🛫

---
+++ pr-preview/pr-77/assets/NavigationUtil-DmfK5sOV.js
@@ -0,0 +1,2 @@
+const t={URLS:{FAQ:"https://walletconnect.com/faq"}};export{t as N};
+//# sourceMappingURL=NavigationUtil-DmfK5sOV.js.map
---
+++ pr-preview/pr-77/assets/transactions-D9bbPcHe.js
@@ -0,0 +1,17 @@
+import{dn as f,dl as a,dm as m}from"./main-fx3iUpV5.js";import{a as p}from"./index-Btv519H4.js";import"./index-BjAbrH0f.js";import"./class-map-DAcImrql.js";import"./index-BiSjf8Ac.js";import"./if-defined-D-gtNDpg.js";import"./index-CBcQ79AI.js";import"./index-5ZJ5ht5H.js";import"./index-C4CrNo00.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var s=o.length-1;s>=0;s--)(l=o[s])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let c=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};c.styles=d;c=u([p("w3m-transactions-view")],c);export{c as W3mTransactionsView};
+//# sourceMappingURL=transactions-D9bbPcHe.js.map
